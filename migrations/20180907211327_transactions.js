d83bb137fa7d22a3384954a4fce904301e394d39
Agosh Saini <agosh.saini@gmail.com>
2018-09-07 22:29:30 -0400

    added tables

--- migrations/20180907211327_transactions.js
+++ migrations/20180907211327_transactions.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = knex => knex.schema.createTable('Transactions', (t) => {
+    t.string('txNum').primary();
+    t.string('to').notNubable();
+    t.string('from').notNubable();
+    t.assetCode('string').notNubable();
+    t.integer('status').defaultTo(0);
+    t.string('txid').defaultTo('notAssigned');
+    t.string('seqid').defaultTo('notAssigned');
+    t.timestamps(true, true);
+});
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = knex => knex.schema.dropTableIfExists('Transactions');
--- migrations/20180907211343_accounts.js
+++ migrations/20180907211343_accounts.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = knex => knex.schema.createTable('Accounts', (t) => {
+  t.string('accountId').primary();
+  t.string('FirstName').notNullable();
+  t.string('LastName').notNullable();
+  t.string('address').notNullable();
+  t.string('House:1').defaultTo(0);
+  t.string('House:3').defaultTo(0);
+  t.timestamp(true, true);
+})
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = knex => knex.schema.dropTableIfExists('Accounts');
